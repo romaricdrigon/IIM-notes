ea1882251bb46a8d9ac231fde7a16692a9704848
Romaric Drigon <romaric@netinfluence.ch>
2015-11-18 23:58:59 +0100

    Added unit test

---
+++ src/AppBundle/Tests/MathTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\Tests;
+
+class MathTests extends \PHPUnit_Framework_TestCase
+{
+    public function test_it_adds()
+    {
+        $this->assertEquals(3, 1+2);
+    }
+}
